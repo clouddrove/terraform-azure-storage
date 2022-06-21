0f95d346a3cfe2c1f70eb97c38e0ef796e1eae38
Mamraj Yadav <mamraj.yadav@clouddrove.com>
2022-06-21 16:37:57 +0530

    Initial commit

--- _example/complete/main.tf
+++ _example/complete/main.tf
@@ -1,1 +1,31 @@
-locals {}
+# Azure Provider configuration
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+  source = "clouddrove/resource-group/azure"
+
+  label_order = ["name", "environment", ]
+  name        = "trustspherstoraget"
+  environment = "staging"
+  location    = "North Europe"
+}
+
+module "storage" {
+  depends_on               = [module.resource_group]
+  source                   = "./../.././"
+  resource_group_name      = module.resource_group.resource_group_name
+  storage_account_name     = "storagestartac"
+  account_kind             = "BlobStorage"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  containers_list = [
+    { name = "mystore250", access_type = "private" },
+
+  ]
+
+}
+
+
---
+++ _example/complete/outputs.tf
@@ -0,0 +1,9 @@
+output "storage_account_id" {
+  value       = module.storage.storage_account_id
+  description = "The ID of the storage account."
+}
+
+output "storage_account_name" {
+  value       = module.storage.storage_account_name
+  description = "The name of the storage account."
+}
