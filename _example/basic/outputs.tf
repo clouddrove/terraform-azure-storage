6883a83b295a542f6534fc6baf6136107844bc8a
Archit Chopra <chopra13archit@gmail.com>
2023-07-06 14:45:03 +0530

    fix: Updated example folder hierarchy and dependabot.yml

---
+++ _example/basic/outputs.tf
@@ -0,0 +1,24 @@
+output "storage_account_id" {
+  value       = module.storage.default_storage_account_id
+  description = "The ID of the storage account."
+}
+
+output "storage_account_name" {
+  value       = module.storage.default_storage_account_name
+  description = "The name of the storage account."
+}
+
+output "file_shares" {
+  description = "storage SMB file shares list"
+  value       = module.storage.file_shares
+}
+
+output "tables" {
+  description = "storage tables list"
+  value       = module.storage.tables
+}
+
+output "queues" {
+  description = "storage queues list"
+  value       = module.storage.queues
+}
