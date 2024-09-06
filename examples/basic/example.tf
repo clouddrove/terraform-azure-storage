df2b5886a288f657045a53c413f777d6cd2d7ec7
Hitman <vedantkalia@clouudrove.com>
2024-09-06 16:11:46 +0530

    Removed Depriciated Value with and Fixed Provider Configuration

--- examples/basic/example.tf
+++ examples/basic/example.tf
@@ -11,7 +11,6 @@ provider "azurerm" {
   subscription_id            = "01111111111110-11-11-11-11"
   skip_provider_registration = "true"
 }
-configuration_aliases = [azurerm.main_sub, azurerm.dns_sub]
 
 locals {
   name        = "app"
